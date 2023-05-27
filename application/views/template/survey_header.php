9cd9c19327a6943c5d896ae0bebc38e0e2cde3aa
unknown <yunanto.arfan19@gmail.com>
2023-05-27 09:09:08 +0700

    Fajar 27-05-2023

--- application/views/template/survey_header.php
+++ application/views/template/survey_header.php
@@ -19,7 +19,6 @@
 	<!-- Custom styles for this template-->
 	<link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 	<link href="<?= base_url('assets/css/css-new.css'); ?>" rel="stylesheet">
-
 </head>
 
 <body class="bg-light">
