f7eaefe4cf35984f57918e6591f265da2267696f
fajardika <fajardikadwiyulianto22@gmail.com>
2023-06-15 08:58:24 +0700

    tambah assets js (pindah dai view survey)

---
+++ assets/js/igd.js
@@ -0,0 +1,19 @@
+function show1(kondisi1) {
+    $('#tap1').css('display', kondisi1);
+}
+
+function show2(kondisi2) {
+    $('#tap2').css('display', kondisi2);
+}
+
+function show3(kondisi3) {
+    $('#tap3').css('display', kondisi3);
+}
+
+function show4(kondisi4) {
+    $('#tap4').css('display', kondisi4);
+}
+
+function show5(kondisi5) {
+    $('#tap5').css('display', kondisi5);
+}
---
+++ assets/js/ranap.js
@@ -0,0 +1,11 @@
+function show5(kondisi5) {
+    $('#tap5').css('display', kondisi5);
+}
+
+function show6(kondisi6) {
+    $('#tap6').css('display', kondisi6);
+}
+
+function show7(kondisi7) {
+    $('#tap7').css('display', kondisi7);
+}
