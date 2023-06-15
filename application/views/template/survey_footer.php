d455083d4204cdb089b089876b976145d118f9c6
fajardika <fajardikadwiyulianto22@gmail.com>
2023-06-15 09:02:08 +0700

    tambah script untuk menampilkan dokter

--- application/views/template/survey_footer.php
+++ application/views/template/survey_footer.php
@@ -16,3 +16,24 @@
 <script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
 
 
+<script>
+			$(document).ready(function() {
+			$('#spesialis').change(function() {
+				var kd_sps = $(this).val();
+				// console.log(kd_sps);
+				$.ajax({
+					type: "POST",
+					url: "<?= base_url('Survey/isi_dokter') ?>",
+					data: {
+						kd_sps: kd_sps
+					},
+					dataType: "JSON",
+					success: function(response) {
+						// console.log(response);
+						$('#dokter').html(response);
+					}
+				})
+			});
+		});
+</script>
+
